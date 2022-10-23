3f1748ffefc7dcbe5b01312b9fe8217fff608353
Laura Rebelo <laura.b.rebelo@gmail.com>
2022-10-23 18:13:35 +0200

    Initial commit

---
+++ GrpcFromJava/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using GrpcFromJava;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("http://localhost:8080", new GrpcChannelOptions
+{
+    UnsafeUseInsecureChannelCallCredentials = true
+});
+
+var client = new HelloService.HelloServiceClient(channel);
+
+
+var reply = await client.helloAsync(
+    new HelloRequest { FirstName = "Laurex", LastName = "Dopex"});
+Console.WriteLine("Response: " + reply.Greeting);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
